c7c6c28521fa28adf14521ac735ce3c34345db66
Tarmity Flassman <tarmity.flassman@gmail.com>
2020-08-15 15:07:09 +1000

    created all methods required in the orm file

--- config/orm.js
+++ config/orm.js
@@ -30,4 +30,58 @@ const objToSql = (ob) => {
     }
     // translate array of strings to a single comma-seperated string
     return arr.toString();
-} 
+};
+
+// Object for all our SQL statement functions.
+const orm = {
+    all: (tableInput, cb) => {
+        const queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    create: (table, cols, vals, cb) => {
+        let queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, (err, results) => {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+
+    update: (table, objColVals, condition, cb) => {
+        let queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString +=  condition;
+
+        console.log(queryString);
+        connection.query(queryString, (err, result) => {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    }
+};
+
+// Export the orm object for the model
+module.exports = orm;
