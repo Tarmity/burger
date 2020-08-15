9847eca8a00c298c6718cda11973f2ead3b15e8f
Tarmity Flassman <tarmity.flassman@gmail.com>
2020-08-15 14:04:57 +1000

    fixed the database name to the correct one burgers_db

---
+++ config/connection.js
@@ -0,0 +1,11 @@
+// Set up MySql connection
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burgers_db"
+});
+
