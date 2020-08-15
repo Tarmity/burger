9e1b7a68c34b87cfec43356486e0fd339b568aa3
Tarmity Flassman <tarmity.flassman@gmail.com>
2020-08-15 14:49:27 +1000

    added helper function to convert ket obj to string

--- config/orm.js
+++ config/orm.js
@@ -12,3 +12,22 @@ const printQuestionMarks = (num) => {
     return arr.toString();
 };
 
+// Help function to convert object key/value pairs to SQl syntax.
+const objToSql = (ob) => {
+    const arr = [];
+
+    // loop through the key and push the key/value as a string int arr.
+    for (const key in ob) {
+        let value = ob[key];
+        //check to skip hidden properties
+        if(Object.hasOwnProperty.call(ob, key)) {
+            // If string with spaces, ad quotations 
+            if(typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+    // translate array of strings to a single comma-seperated string
+    return arr.toString();
+} 
