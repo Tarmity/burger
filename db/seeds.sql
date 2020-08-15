1cec8a70b7595559e4c9f24a292732843ea03e28
Tarmity Flassman <tarmity.flassman@gmail.com>
2020-08-15 13:55:51 +1000

    fixed the syntax in the seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,7 @@
-INSERT INTO burgers (name) VALUES ('Origianl Angus');
-INSERT INTO burgers (name) VALUES ('Western Bacon Angus');
-INSERT INTO burgers (name, devour) VALUES ('Bacon Chesseburger', true);
-INSERT INTO burgers (name, devour) VALUES ('Chicken Club', true);
-INSERT INTO burgers (name, devour) VALUES ('Veggie Star Burger', true);
-INSERT INTO burgers (name) VALUES ('Brekkie Sausage');
+INSERT INTO burgers (`burger_name`) VALUES ('Origianl Angus');
+INSERT INTO burgers (`burger_name`) VALUES ('Western Bacon Angus');
+INSERT INTO burgers (`burger_name`, devoured) VALUES ('Bacon Chesseburger', true);
+INSERT INTO burgers (`burger_name`, devoured) VALUES ('Chicken Club', true);
+INSERT INTO burgers (`burger_name`, devoured) VALUES ('Veggie Star Burger', true);
+INSERT INTO burgers (`burger_name`) VALUES ('Brekkie Sausage');
+
