2534af98656848ebea4597896a23a7e10d2923b1
Tarmity Flassman <tarmity.flassman@gmail.com>
2020-08-15 13:33:47 +1000

    added the insert info

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Origianl Angus');
+INSERT INTO burgers (name) VALUES ('Western Bacon Angus');
+INSERT INTO burgers (name, devour) VALUES ('Bacon Chesseburger', true);
+INSERT INTO burgers (name, devour) VALUES ('Chicken Club', true);
+INSERT INTO burgers (name, devour) VALUES ('Veggie Star Burger', true);
+INSERT INTO burgers (name) VALUES ('Brekkie Sausage');
