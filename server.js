6350c5ba4ec1160868a4ea1927f98cdc5d2e27d0
Tarmity Flassman <tarmity.flassman@gmail.com>
2020-08-17 09:55:34 +1000

    add the functions and promises to ther file

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
 const express = require("express");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.stack("public"));
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ default: "main "}));
+app.set("view engine", "handlebars");
+
+const routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, () => {
+    consoler.log("Server listening on://localhost:" + PORT);
+})
