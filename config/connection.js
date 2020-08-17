1f0ed80e80fee2b5e41bb8089eba1a98458f7c54
Tarmity Flassman <tarmity.flassman@gmail.com>
2020-08-17 10:10:09 +1000

    fixed a bug added the layout for the template

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "Boston@29",
     database: "burgers_db"
 });
 
