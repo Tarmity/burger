544cf957927938d6e40d6108bc9085433d7004a1
Tarmity Flassman <tarmity.flassman@gmail.com>
2020-08-18 18:12:22 +1000

    fix the connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 // Set up MySql connection.
 const mysql = require("mysql");
+let connection;
 
-const connection = mysql.createConnection({
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+    connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
     user: "root",
     password: "Boston@29",
     database: "burgers_db"
 });
+};
 
 // Make connection. 
 connection.connect((err) => {
@@ -20,4 +24,3 @@ connection.connect((err) => {
 
 // Export connection for our ORM to use.
 module.exports = connection;
-
